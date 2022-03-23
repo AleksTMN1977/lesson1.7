3d817620be5df7d11395e91306ea819df84a3d54
AleksTMN <kirilloff_77@mail.ru>
2022-03-23 18:28:04 +0500

    3 задача

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,10 +3,26 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	task1();
+        task2();
+
     }
 
     public static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName1);
+    }
+
+    public static void task2() {
+        String fullName = "Иванов Семён Семёнович";
+        String[] words = fullName.split(" ");
+        for (String word : words) {
+            System.out.print(word);
+        }
 
     }
 }
